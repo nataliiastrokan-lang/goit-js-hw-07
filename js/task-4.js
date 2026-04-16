4afb8ca1b5e3823af4d1df90312efb12fe620cb9
Nataliia Strokan <nataliia.strokan@gmail.com>
2026-04-16 18:56:56 +0300

    task-4 login

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", event => {
+  event.preventDefault(); // ❗ не перезавантажує сторінку
+
+  const { email, password } = event.target.elements;
+
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+
+  // перевірка
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  // створюємо об'єкт
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+
+  // очищаємо форму
+  form.reset();
+});
